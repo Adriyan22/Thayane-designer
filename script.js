a91b0268ad3cf1f24e825a5d49b4aca3bb20027a
Adriyan Web Developer <145672511+Adriyan22@users.noreply.github.com>
2024-11-20 09:05:18 -0300

    meu segundo job como desenvolvedor junior

---
+++ script.js
@@ -0,0 +1,16 @@
+let menuItems = document.querySelector('.inform-items')
+
+function exibirEocultar() {
+    menuItems.classList.toggle('inform-items')
+    menuItems.classList.add('menu-mobile')
+    menuItems.classList.add('referencia')
+}
+
+
+
+
+
+
+
+
+
